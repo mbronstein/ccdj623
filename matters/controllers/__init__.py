74717e6e0a8e8c30a8b512f8350f269bda7a41fd
mbronstein <mark@bronsteinlaw.com>
2022-07-15 22:01:02 -0400

    get rid of ninja schema and controller dirs in matters for now

--- matters/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .store_book_subscriptions import StoryBookSubscribeController
-from .matters import MattersController #, StoreBookController,
-
-__all__ = [ "MattersController"]
--- matters/schemes/__init__.py
+++
@@ -1,1 +0,0 @@
-from ..schemes.matters import MatterSchema, CreateMatterSchema, MatterRetrieveSchema
